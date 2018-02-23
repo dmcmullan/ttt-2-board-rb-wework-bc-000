2ff10d1c4710bf2c21d202adacae0408b79c7f8f
Dominic McMullan <dominic.mcmullan@wework.com>
2018-02-23 21:56:55 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,15 @@
 # Define the variable board below.
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+puts "This is not part of the method"
+display_board
+display_board
+display_board
